a22338c5cc0bf5a8d40090e1877992d369ab79f5
irms19 <irms19@7330a5c5-07fc-53ea-e431-edd981856d35>
2011-03-25 13:37:16 +0000

    Clarissa's Changes to Parser.cpp and .h

--- trunk/Calculator/Parser.cpp
+++ trunk/Calculator/Parser.cpp
@@ -4,49 +4,59 @@
 
 #include "Parser.h"
 
-Parser::Parser(vector<Token> _input): input(_input) {
-	TokenEnd end = TokenEnd(); ////fix
-	opStack.push_back(end);
-
+Parser::Parser(vector<Token*> _input): input(_input) {
+        TokenEnd end = TokenEnd(); ////fix
+        opStack.push_back(end);
 }
 
 Parser::~Parser(){}
 
 /*Member Functions*/
 
-list<Token> Parser::getPostFix() const{
-	return postFix;
+list<Token*> Parser::getPostFix() const{
+        return postFix;
 }
 
-list<Token> Parser::getOpStack() const{
-	return opStack;
+list<Token*> Parser::getOpStack() const{
+        return opStack;
 }
 
 void Parser::infixToPostfix(){
-	for (int i = 0; i < input.size(); i++){
-		Token inp = input[i];
-		if (inp is a TokenOperand){ 
-			postFix.enqueue(inp);
-		}
-		if(inp is TokenEnd){
-			pop everything up to the END token and enqueue
-		}
-		if(inp is a TokenOpenParen){
-			opStack.push(inp);
-		}
-		if(inp is a TokenCloseParen){
-			pop everything up to left paren (ignore paren)  and enqueue
-		}
-		Token temp = opStack.pop();
-		if(inp.inPriors >= temp.stackPriors){
-			opStack.push(inp);
-		}
-		else{
-			postFix.enqueue(inp);
-		
-		}
-	}
+        for (int i = 0; i < input.size(); i++){
+                Token* inp = input[i];
+                if (inp->getId()=='d'){ 
+                        postFix.push_back(inp);
+                }
+                if(inp->getId()=='#'){
+					while (!opStack.empty() && ((opStack.back())->getId())!='#') {
+						postFix.push_back(opStack.back());
+						opStack.pop_back();
+                       // pop everything up to the END token and enqueue
+					}
+					//pop #?
+                }
+                if(inp is a TokenOpenParen){
+                        opStack.push_back(inp);
+                }
+                if(inp is a TokenCloseParen){
+					while (!opStack.empty() && ((opStack.back())->getId())!='(') {
+						postFix.push_back(opStack.back());
+						opStack.pop_back();
+						//pop everything up to left paren (ignore paren)  and enqueue
+					}
+					if (!opStack.empty() && ((opStack.back())->getId())=='(') {
+						opStack.pop_back();
+					}
+                }
+                *Token temp = opStack.front();
+                if(inp->getInPrior() >= temp->getStackPrior()) {
+                        opStack.push(inp);
+                }
+                else{
+                        postFix.enqueue(inp);
+                
+                }
+        }
 
 }
 
-	
